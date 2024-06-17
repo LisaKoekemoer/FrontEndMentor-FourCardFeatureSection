460be6f70fb9c435d4d68550968c605e8b364e8f
Alicia <alicia.koekemoer98@gmail.com>
2024-06-17 13:50:36 +0200

    Completed Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import supervisorIcon from './images/icon-supervisor.svg';
+import teambuilderIcon from './images/icon-team-builder.svg';
+import karmaIcon from './images/icon-karma.svg';
+import calculatorIcon from './images/icon-calculator.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header-section'>
+        <h1 className='header'>Reliable, efficient delivery 
+        <span> Powered by Technology</span></h1> 
+
+        <p>Our Artificial Intelligence powered tools use millions of 
+           project data points to ensure that your project is successful</p>
+      </div>
+
+      <div className='feature-section'>
+        <div className='supervisor'>
+          <h1>Supervisor</h1>
+          <p>Monitor activity to identify project roadblocks</p>
+          <img src={supervisorIcon} alt=""/>
+        </div>
+        <div className='tower'>
+          <div className='team-builder'>
+            <h1>Team Builder</h1>
+            <p>Scans our talent network to create the optimal tea, for your project</p>
+            <img src={teambuilderIcon} alt=""/>
+          </div>
+          <div className='karma'>
+            <h1>Karma</h1>
+            <p>Regularly evaluates our talent to ensure quality</p>
+            <img src={karmaIcon} alt=""/>
+          </div>
+        </div>
+        <div className='calculator'>
+          <h1>Calculator</h1>
+          <p>Uses data from past projects to provide better delivery estimates</p>
+          <img src={calculatorIcon} alt=""/>
+        </div>
+      </div>
+
+    </>
   );
 }
 
